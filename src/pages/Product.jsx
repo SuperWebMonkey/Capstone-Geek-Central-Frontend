a6ef465615274fc97c3a23fa088f0522b4a2c38e
SuperWebMonkey <vikingsattack@outlook.com>
2024-11-20 21:15:44 -0800

    some more of the search feature

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,16 +5,18 @@ import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 // import Cart from "./Cart.jsx";
 
-const api_url = "https://digimon-api.vercel.app/api/digimon";
+const api_url = "http://localhost:3000/products";
 
 function Product() {
+  const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
   const [loadApi, setLoading] = useState(true);
   const [allProducts, setAllProducts] = useState([]);
+  const [filterProducts, setFilter] = useState([]);
 
   const fetchProducts = async () => {
     try {
-      const res = await axios.get("http://localhost:3000/products");
+      const res = await axios.get(api_url);
       console.log(res.data);
       setAllProducts(res.data);
     } catch (e) {
@@ -27,20 +29,29 @@ function Product() {
     fetchProducts(), [];
   });
 
-  // Use effect for getting the digi api
-  useEffect(() => {
-    axios
-      .get(api_url)
-      .then((res) => {
-        // setDigimons(res.data);
-        // setFilter(res.data);
-        // setLoading(false);
-      })
-      .catch((e) => {
-        console.error("Error when fetching Digimon data:", e);
-        // setLoading(false);
-      });
-  }, []);
+  // Search Function
+  const searchFeature = (e) => {
+    e.preventDefault();
+    const query = e.target.value.toLowerCase();
+    // console.log(typeof query, query);
+    setSearch(query);
+
+    const filteredList = products.filter((item) => {
+      const titleMatch = item.title.toLowerCase().includes(query);
+
+      // console.log(`Searching for: ${query}`);
+      // console.log(
+      //   `Checking Title: ${item.title.toLowerCase()} (title match: ${titleMatch})`
+      // );
+
+      return titleMatch;
+    });
+
+    // console.log(products);
+    // console.log(filteredList);
+
+    setFilter(filteredList);
+  };
 
   return <></>;
 }
